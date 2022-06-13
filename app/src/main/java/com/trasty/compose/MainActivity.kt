f4ea0532e08734823beddadce49bc2ad2bae42ba
TemirlanKN <temirlan9908@gmail.com>
2022-06-13 15:52:10 +1300

    Box modifier with clickable functionality

--- app/src/main/java/com/trasty/compose/MainActivity.kt
+++ app/src/main/java/com/trasty/compose/MainActivity.kt
@@ -19,6 +19,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,46 +28,81 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.trasty.compose.ui.theme.ComposeTheme
+import kotlin.text.Typography.less
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier.verticalScroll(rememberScrollState())
-            ){
-                ListItem("Damir", "Handyman")
-                ListItem("Aida", "Photograph")
-                ListItem("Nurlan", "Singer")
-                ListItem("Someone", "Someone")
+//            Column(
+//                modifier = Modifier.verticalScroll(rememberScrollState())
+//            ){
+//                ListItem("Damir", "Handyman")
+//                ListItem("Aida", "Photograph")
+//                ListItem("Nurlan", "Singer")
+//                ListItem("Someone", "Someone")
+//            }
+            Box(modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center) {
+                CircleItem()
             }
         }
     }
 }
 
 @Composable
+private fun CircleItem(){
+    var counter = remember {
+        mutableStateOf(0)
+    }
+    var color = remember {
+        mutableStateOf(Color.Blue)
+    }
+    Box(modifier = Modifier
+        .size(100.dp)
+        .background(color = color.value, shape = CircleShape)
+        .clickable {
+            when (++counter.value % 3) {
+                0 -> color.value = Color.Red
+                1 -> color.value = Color.Green
+                2 -> color.value = Color.Black
+            }
+        },
+            contentAlignment = Alignment.Center){
+            Text(text = counter.value.toString(),
+            style = TextStyle(color = Color.White, fontSize = 20.sp)
+            )
+    }
+}
+
+@Composable
 private fun ListItem(name: String, prof: String){
+    var color = remember {
+        mutableStateOf(Color.White)
+    }
+    var counter = remember {
+        mutableStateOf(0)
+    }
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(10.dp)
-            .pointerInput(Unit){
-                detectHorizontalDragGestures { change, dragAmount ->
-                    Log.d("MyLog", "Long press $dragAmount")
-                }
-
-
-            }
             .clickable {
-                Log.d("MyLog", "Clicked $name")
+                when (++counter.value) {
+                    10 -> color.value = Color.Cyan
+                    20 -> color.value = Color.LightGray
+                }
+//                Log.d("MyLog", "Counter for $name = $counter")
             },
         shape = RoundedCornerShape(15.dp),
         elevation = 5.dp
     ){
-        Box(){
+        Box(modifier = Modifier.background(color = color.value)){
             Row(verticalAlignment = Alignment.CenterVertically){
                 Image(
                     painter = painterResource(id =
@@ -86,6 +123,7 @@ private fun ListItem(name: String, prof: String){
                 Column(
                     modifier = Modifier.padding(start = 16.dp)
                 ) {
+                    Text(text = counter.value.toString())
                     Text(text = name)
                     Text(text = prof)
                 }
